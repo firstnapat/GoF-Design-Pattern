db05ad239277efa64e479ee741dc1767c54f361c
firstnapat <napatdmsu@gmail.com>
2023-10-30 00:27:08 +0700

    learn observer pattern

---
+++ code/src/observer/lab_first/weatherStation.test.ts
@@ -0,0 +1,53 @@
+import { ConditionDisplay } from './conditionDisplay';
+import { ForecastDisplay, StatisticDisplay } from './statisticDisplay';
+import { WeatherData } from './weatherData';
+import { WeatherStation } from './weatherStation';
+
+describe('[Observer - begin] Weather station measurement changed', () => {
+  it('display should be called when measurement changed', () => {
+    // given
+    const weatherStation = new WeatherStation();
+    const data = new WeatherData(32.0, 0.8, 1.0);
+
+    // when
+    weatherStation.update(data);
+
+    // then
+    expect(weatherStation.updateCurrentConditionsDisplay()).toBe(
+      'Current conditions: 32C degrees and 0.8% humidity'
+    );
+    expect(weatherStation.updateStatisticsDisplay()).toBe(
+      'Avg/Max/Min temperature = 32/0.8/1'
+    );
+    expect(weatherStation.updateForecastDisplay()).toBe(
+      'Forecast: More of the same'
+    );
+  });
+
+  it('it should display condition when weather station data changed', () => {
+    // given
+    const weatherStation = new WeatherStation();
+    const data = new WeatherData(32.0, 0.8, 1.0);
+    
+    const conditionDisplay = new ConditionDisplay(); 
+    const statisticDisplay = new StatisticDisplay(); 
+    const forecastDisplay = new ForecastDisplay(); 
+    
+    weatherStation.registerObserver(conditionDisplay);
+    weatherStation.registerObserver(statisticDisplay);
+    weatherStation.registerObserver(forecastDisplay);
+    // when
+    weatherStation.update(data);
+
+    // then
+    expect(conditionDisplay.display()).toBe(
+      'Current conditions: 32C degrees and 0.8% humidity'
+    );
+    expect(statisticDisplay.display()).toBe(
+      'Avg/Max/Min temperature = 32/0.8/1'
+    );
+    expect(forecastDisplay.display()).toBe(
+      'Forecast: More of the same'
+    );
+  });
+});
---
+++ code/src/observer/lab_first/weatherStation.ts
@@ -0,0 +1,28 @@
+import { WeatherData } from './weatherData';
+import { WeatherObserver } from './weatherObserver';
+
+export class WeatherStation {
+  private observers: WeatherObserver[] = [];
+
+  public registerObserver(observer: WeatherObserver): void {
+    this.observers.push(observer);
+  }
+  private data = new WeatherData(0, 0, 0);
+
+  public update(data: WeatherData): void {
+    this.data = data;
+    this.observers.forEach((observers) => observers.update(data));
+  }
+
+  updateCurrentConditionsDisplay(): string {
+    return `Current conditions: ${this.data.temperature}C degrees and ${this.data.humidity}% humidity`;
+  }
+
+  updateStatisticsDisplay(): string {
+    return `Avg/Max/Min temperature = ${this.data.temperature}/${this.data.humidity}/${this.data.pressure}`;
+  }
+
+  updateForecastDisplay(): string {
+    return `Forecast: More of the same`;
+  }
+}
